cae241b42d2bf273dbb0affbf4d771d057043458
Rubén <ruben.talon.argente@gmail.com>
2025-09-17 16:23:29 +0200

    Add files via upload

---
+++ storybook-static/assets/Button.stories-C-p99DyT.js
@@ -0,0 +1,25 @@
+import{B as t}from"./Button-DqOSXEXc.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-Cigh3cB9.js";import"./preload-helper-D9Z9MdNV.js";import"./index-C3CZAKeI.js";const{fn:n}=__STORYBOOK_MODULE_TEST__,i={title:"Example/Button",component:t,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{backgroundColor:{control:"color"}},args:{onClick:n()}},r={args:{primary:!0,label:"Button"}},a={args:{label:"Button"}},e={args:{size:"large",label:"Button"}},o={args:{size:"small",label:"Button"}},s={args:{primary:!1,label:"Button"}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: true,
+    label: 'Button'
+  }
+}`,...r.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    label: 'Button'
+  }
+}`,...a.parameters?.docs?.source}}};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'large',
+    label: 'Button'
+  }
+}`,...e.parameters?.docs?.source}}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {
+    size: 'small',
+    label: 'Button'
+  }
+}`,...o.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    primary: false,
+    label: "Button"
+  }
+}`,...s.parameters?.docs?.source}}};const d=["Primary","Secondary","Large","Small","Tutorial"];export{e as Large,r as Primary,a as Secondary,o as Small,s as Tutorial,d as __namedExportsOrder,i as default};
