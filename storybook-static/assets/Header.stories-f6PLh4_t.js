c92792ef97edc5d5436ecf89ca99b6e1660a93a3
DEPID\rtalon <rtalon.etraid@grupoetra.com>
2025-09-18 07:31:36 +0200

    un desastre de curso...

--- storybook-static/assets/Header.stories-f6PLh4_t.js
+++
@@ -1,7 +0,0 @@
-import{H as a}from"./Header-BS5MAHMg.js";import"./jsx-runtime-D_zvdyIk.js";import"./iframe-Cigh3cB9.js";import"./preload-helper-D9Z9MdNV.js";import"./index-C3CZAKeI.js";import"./Button-DqOSXEXc.js";const{fn:o}=__STORYBOOK_MODULE_TEST__,d={title:"Example/Header",component:a,tags:["autodocs"],parameters:{layout:"fullscreen"},args:{onLogin:o(),onLogout:o(),onCreateAccount:o()}},e={args:{user:{name:"Jane Doe"}}},r={};e.parameters={...e.parameters,docs:{...e.parameters?.docs,source:{originalSource:`{
-  args: {
-    user: {
-      name: 'Jane Doe'
-    }
-  }
-}`,...e.parameters?.docs?.source}}};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};const g=["LoggedIn","LoggedOut"];export{e as LoggedIn,r as LoggedOut,g as __namedExportsOrder,d as default};
